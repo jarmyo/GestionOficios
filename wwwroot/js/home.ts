64fd628c2a1a391966f03b144bc23534ee83ffd3
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2021-06-07 17:48:51 -0500

    notificacion

---
+++ wwwroot/js/home.ts
@@ -0,0 +1,27 @@
+﻿if (!window.Notification) {
+    console.log('Browser does not support notifications.');
+} else {
+    // check if permission is already granted
+    if (Notification.permission === 'granted') {
+        
+    } else {
+        // request permission from user
+        Notification.requestPermission().then(function (p) {
+            if (p === 'granted') {
+             
+            } else {
+                console.log('User blocked notifications.');
+            }
+        }).catch(function (err) {
+            console.error(err);
+        });
+    }
+}
+
+function notificar(titulo: string, mensaje: string) {
+    var notify = new Notification(titulo, {
+        body: mensaje,
+        icon: "/favicon.ico"
+    });
+       
+}
